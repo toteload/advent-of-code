df057a329fef31bc37cc7460096de8dee6275bcf
toteload <toteload@users.noreply.github.com>
2024-12-16 22:17:11 +0100

    Ugly day 16 part 2

--- 2024/day16.py
+++ 2024/day16.py
@@ -1,4 +1,5 @@
 import fileinput
+from collections import defaultdict
 from itertools import pairwise
 from dataclasses import dataclass, astuple
 from heapq import heappop, heappush
@@ -49,19 +50,78 @@ end = grid.find(lambda x: x == 'E')
 directions = [Point(1,0), Point(0,-1), Point(-1,0), Point(0,1)]
 
 def neighbors(r):
-    res = []
     np = r.p + r.facing
     if grid.at_p(np) != '#':
-        res.append(Reindeer(np, r.facing))
-    res.append(Reindeer(r.p, directions[directions.index(r.facing)-3]))
-    res.append(Reindeer(r.p, directions[directions.index(r.facing)-1]))
-    return res
+        yield Reindeer(np, r.facing)
+    yield Reindeer(r.p, directions[directions.index(r.facing)-3])
+    yield Reindeer(r.p, directions[directions.index(r.facing)-1])
 
 def cost(s, t):
     if s.facing.x != t.facing.x:
         return 1000
     return 1
 
-path = astar_search(Reindeer(start, Point(1,0)), lambda x: manhattan(astuple(x.p), astuple(end)), neighbors, cost)
+def heuristic(a):
+    d = manhattan(astuple(a.p), astuple(end))
+    if a.facing == Point(-1,0) or a.facing == Point(0,1):
+        return d + 2000
+    #if (a.p.x == end.x and a.facing != Point(0,-1)) or (a.p.y == end.y and a.facing != Point(1, 0)):
+    #    return d + 1000
+    return d
 
-print(sum(cost(a,b) for a,b in pairwise(path)))
+path = astar_search(Reindeer(start, Point(1,0)), heuristic, neighbors, cost)
+
+best = sum(cost(a,b) for a,b in pairwise(path))
+print(best)
+
+def trace_tiles(prev, at):
+    if not prev[at]:
+        return set()
+
+    s = {at.p}
+    for x in prev[at]:
+        s |= trace_tiles(prev, x)
+    return s
+
+def astar_search_iter(start, heuristic, neighbors, stepcost=lambda a,b:1):
+    frontier = [(heuristic(start), start)]
+    prev = defaultdict(set)
+    path_cost = {start: 0}
+
+    while frontier:
+        (f, s) = heappop(frontier)
+
+        if heuristic(s) == 0 and f > best:
+            return
+
+        if heuristic(s) == 0:
+            yield trace_tiles(prev, s)
+            continue
+
+        for t in neighbors(s):
+            tscore = path_cost[s] + stepcost(s, t)
+            if t not in path_cost or tscore <= path_cost[t]:
+                heappush(frontier, (tscore + heuristic(t), t))
+                path_cost[t] = tscore
+                prev[t].add(s)
+
+    return None
+
+tiles = None
+
+# There is a bug in my dirty code.
+# For the examples it returns the correct answer, but for some reason it is off by one for the real
+# input. When I draw the visited tiles in the grid, the starting tile is not marked. It is marked
+# when I run it for the examples... I don't know what goes wrong and I'm too tired to care.
+# Just add one :)
+for t in astar_search_iter(Reindeer(start, Point(1,0)), heuristic, neighbors, cost):
+    tiles = t
+
+m = [list(line) for line in grid.compact_str().split('\n')]
+for r in tiles:
+    c = 'O'
+    m[r.y][r.x] = c
+print('\n'.join([''.join(line) for line in m]))
+
+
+print(len(tiles))
