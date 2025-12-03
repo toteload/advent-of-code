508d95b68f1fc2497a8ca6aacae806274361d1e0
toteload <toteload@users.noreply.github.com>
2025-12-03 21:42:11 +0100

    Simplify day 3

--- 2025/day03.c
+++ 2025/day03.c
@@ -12,6 +12,19 @@ i32 find_max_idx(char const *xs, i32 lo, i32 hi) {
   return idx;
 }
 
+i64 joltage(char const *buf, i32 len, i32 ndigits) {
+  i64 acc = 0;
+
+  i32 lo = 0;
+  for (i32 i = 0; i < ndigits; i++) {
+    i32 idx = find_max_idx(buf, lo, len - (ndigits - 1 - i));
+    acc = (10 * acc) + buf[idx];
+    lo = idx + 1;
+  }
+
+  return acc;
+}
+
 i32 main(i32 argc, char const **argv) {
   if (argc < 2) {
     printf("Usage: %s input.txt\n", argv[0]);
@@ -20,59 +33,26 @@ i32 main(i32 argc, char const **argv) {
 
   char *text = read_file(argv[1]);
 
-  // Part one
-  {
-    i64 sum = 0;
-    char buf[100];
-    char *at = text;
-    while (*at) {
-      i32 len = 0; 
-      while (is_number(*at)) {
-        buf[len++] = *at++ - '0';
-      }
+  i64 sum = 0;
+  i64 sum2 = 0;
 
-      i32 i = find_max_idx(buf, 0, len - 1);
-      i32 j = find_max_idx(buf, i + 1, len);
-
-      sum += 10 * buf[i] + buf[j];
-
-      while (*at && !is_number(*at)) {
-        at++;
-      }
+  char buf[100];
+  char *at = text;
+  while (*at) {
+    i32 len = 0; 
+    while (is_number(*at)) {
+      buf[len++] = *at++ - '0';
     }
 
-    printf("%lld\n", sum);
-  }
+    sum  += joltage(buf, len, 2);
+    sum2 += joltage(buf, len, 12);
 
-  // Part two
-  {
-    i64 sum = 0;
-    char buf[100];
-    char *at = text;
-    while (*at) {
-      i32 len = 0; 
-      while (is_number(*at)) {
-        buf[len++] = *at++ - '0';
-      }
-
-      i64 acc = 0;
-
-      i32 lo = 0;
-      for (i32 i = 0; i < 12; i++) {
-        i32 idx = find_max_idx(buf, lo, len - (11 - i));
-        acc = (10 * acc) + buf[idx];
-        lo = idx + 1;
-      }
-
-      sum += acc;
-
-      while (*at && !is_number(*at)) {
-        at++;
-      }
+    while (*at && !is_number(*at)) {
+      at++;
     }
-
-    printf("%lld\n", sum);
   }
+
+  printf("%lld\n%lld\n", sum, sum2);
 
   return 0;
 }
