dc845b7f2ee420f54b6fe88d89464cf1a6abb61e
toteload <toteload@users.noreply.github.com>
2025-12-16 22:11:15 +0100

    Day 8 WIP

--- 2025/day08.c
+++ 2025/day08.c
@@ -1,36 +1,67 @@
 #include "util.h"
 
-typedef struct Connection {
-  i64 distance;
-  i32 idx[2];
-  i8 is_visited;
-};
+#define MAX_BOXES 1000
+#define MAX_CONNECTIONS 1000
 
-Connection *find_insert(Connection *start, Connection *end, i64 d) {
-  // TODO
-  return NULL;
+typedef struct Pair {
+  i32 idx[2];
+} Pair;
+
+typedef struct Box {
+  i32 data[3];
+} Box;
+
+i64 box_distance(Box *a, Box *b) {
+  i64 dx = a->data[0] - b->data[0];
+  i64 dy = a->data[1] - b->data[1];
+  i64 dz = a->data[2] - b->data[2];
+  return dx*dx + dy*dy + dz*dz;
 }
 
-i32 flood(Connection *conns, i32 count, i32 a, i32 b) {
-  if (conns[at].is_visited) {
+Pair *find_insert(Box *box, Pair *start, Pair *end, i64 d) {
+  if (start == end) {
+    return start;
+  }
+
+  i32 len = (i32)(end - start);
+  Pair *mid = start + len / 2;
+
+  i64 d_mid = box_distance(box + mid->idx[0], box + mid->idx[1]);
+
+  if (d < d_mid) {
+    return find_insert(box, start, mid, d);
+  }
+
+  return find_insert(box, mid + 1, end, d);
+}
+
+i32 flood(Pair *pairs, Pair **visited) {
+  if (pairs == *visited) {
     return 0;
   }
 
-  conns[at].is_visited = true;
+  *visited = *visited - 1;
+
+  Pair curr = *pairs;
+
+  swap(*pairs, **visited);
 
   i32 sum = 0;
 
-  for (i32 i = 0; i < count; i++) {
-    if (i == at) {
-      continue;
+  Pair *at = pairs;
+  while (at < *visited) {
+    if (at->idx[0] == curr.idx[0] ||
+        at->idx[0] == curr.idx[1] ||
+        at->idx[1] == curr.idx[0] ||
+        at->idx[1] == curr.idx[1])
+    {
+      sum += flood(pairs, visited);
     }
 
-    if (conns[i].idx[0] == conns[at].idx[0] || conns[i].idx[1] == conns[at].idx[1]) {
-
-    }
+    at++;
   }
 
-  return 0;
+  return 1 + sum;
 }
 
 i32 main(i32 argc, char const **argv) {
@@ -42,53 +73,62 @@ i32 main(i32 argc, char const **argv) {
   char *text = read_file(argv[1]);
   i32 npairs = parse_int(argv[2]);
 
-  i32 boxes[1000 * 3];
+  Box box[MAX_BOXES];
 
   i32 len = 0;
-  i32 *box = boxes;
   char const *at = text;
   while (*at) {
-    box[0] = parse_int_advance(&at);
+    i32 x = parse_int_advance(&at);
     at++; // skip ','
-    box[1] = parse_int_advance(&at);
+    i32 y = parse_int_advance(&at);
     at++; // skip ','
-    box[2] = parse_int_advance(&at);
+    i32 z = parse_int_advance(&at);
+
+    box[len++] = (Box){ .data = { x, y, z, }, };
+
     at = next_line(at);
-    box += 3;
-    len++;
   }
 
-  for (i32 i = 0; i < len - 1; i++) {
-    for (i32 j = i + 1; j < len; j++) {
-      i64 dx = box[i*3] - box[j*3];
-      i64 dy = box[i*3+1] - box[j*3+1];
-      i64 dz = box[i*3+2] - box[j*3+2];
+  Pair pairs[MAX_CONNECTIONS];
+  for (i32 i = 0, k = 0; i < len - 1; i++) {
+    for (i32 j = i + 1; j < len; j++, k++) {
+      i64 d = box_distance(box + i, box + j);
 
-      i64 d = dx*dx + dy*dy + dz*dz;
+      Pair *end = pairs + min(k, npairs);
+      Pair *insert = find_insert(box, pairs, end, d);
 
-      Connection *end = conns + min(i, npairs);
-      Connection *insert = find_insert(conns, end, d);
-      if (insert == end) {
+      if (insert == end && i >= npairs) {
         continue;
       }
 
-      memmove(/* TODO */);
+      end = pairs + min(k+1, npairs);
+      memmove(insert + 1, insert, sizeof(Pair) * (end - insert));
 
-      *insert = (Connection){
-        .distance = d,
-        .idx = { i, j, },
-        .is_visited = 0,
-      };
+      *insert = (Pair){ .idx = { i, j, }, };
     }
   }
 
   for (i32 i = 0; i < npairs; i++) {
-    if (conns[i].is_visited) {
-      continue;
-    }
-
-    i32 size = flood(conns, npairs, i);
+    i64 d = box_distance(box + pairs[i].idx[0], box + pairs[i].idx[1]);
+    printf("distance %lld, %d %d\n", d, pairs[i].idx[0], pairs[i].idx[1]);
   }
+
+  i32 best[3] = { 0, 0, 0, };
+
+  Pair *visited = pairs + npairs;
+  while (visited != pairs) {
+    i32 size = flood(pairs, &visited);
+
+    printf("%d\n", size);
+
+    if (size > best[0]) { best[0] = size; }
+
+    if (best[0] > best[1]) { swap(best[0], best[1]); }
+    if (best[1] > best[2]) { swap(best[1], best[2]); }
+  }
+
+  i64 answer = best[0] * best[1] * best[2];
+  printf("%lld\n", answer);
 
   return 0;
 }
